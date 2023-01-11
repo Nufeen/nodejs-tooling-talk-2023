cd78c077f7ce20c86a73a1046d83357d7bbb4ec4
i.zemlyachenko <i.zemlyachenko@tinkoff.ru>
2023-01-11 13:49:52 +0300

    Pure fastify

---
+++ index.mjs
@@ -0,0 +1,20 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (request, reply) => {
+  return { hello: "world!11" };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
